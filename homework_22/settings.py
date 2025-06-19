d04e2d3dc852f03f50211079e3c4aa9cb6822cc3
Gabdulla <gabdulla2001@gmail.com>
2025-06-20 01:41:14 +0300

    added some security measures

--- homework_22/settings.py
+++ homework_22/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,14 +76,15 @@ WSGI_APPLICATION = "homework_22.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+load_dotenv()
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "db_django",
-        "USER": "postgres",
-        "PASSWORD": "72546725",
-        "HOST": "localhost",
-        "PORT": "5432"
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": os.getenv("DATABASE_HOST"),
+        "PORT": os.getenv("DATABASE_PORT", default=5432)
     }
 }
 
