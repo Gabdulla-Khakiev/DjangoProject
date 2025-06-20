e0314edf851ed50721acb610049b4f6fde3b6759
Gabdulla <gabdulla2001@gmail.com>
2025-06-20 22:05:12 +0300

    added add_product form

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, contacts, product_detail
+from .views import home, contacts, product_detail, add_product
 from .apps import CatalogConfig
 
 app_name = CatalogConfig.name
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', home, name='home'),
     path('home/', home, name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('products/<int:pk>/', product_detail, name='product_detail')
+    path('products/<int:pk>/', product_detail, name='product_detail'),
+    path('add/', add_product, name='add_product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from catalog.models import Product
+from catalog.forms import ProductForm
 
 
 def home(request):
@@ -17,3 +18,14 @@ def contacts(request):
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     return render(request, "product_detail.html", {"product": product})
+
+
+def add_product(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')  # или на страницу товара
+    else:
+        form = ProductForm()
+    return render(request, 'catalog/add_product.html', {'form': form})
