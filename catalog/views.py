0f0cb17b5c583ba031a86c621f1e532f419a8e70
Gabdulla <gabdulla2001@gmail.com>
2025-06-20 22:52:37 +0300

    added add_product.html, pagination, and changed forms styles

--- catalog/views.py
+++ catalog/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from catalog.models import Product
 from catalog.forms import ProductForm
+from django.core.paginator import Paginator
 
 
 def home(request):
-    products = Product.objects.all()
-    return render(request, "home.html", {"products": products})
+    product_list = Product.objects.all().order_by('-created_at')
+    paginator = Paginator(product_list, 6)  # 6 товаров на страницу
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'home.html', {'page_obj': page_obj})
 
 
 def contacts(request):
@@ -25,7 +29,7 @@ def add_product(request):
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('home')  # или на страницу товара
+            return redirect('/')
     else:
         form = ProductForm()
-    return render(request, 'catalog/add_product.html', {'form': form})
+    return render(request, 'add_product.html', {'form': form})
