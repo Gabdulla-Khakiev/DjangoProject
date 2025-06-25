74dd2afbd29fa019af56b0688a66d31555906c0b
Gabdulla <gabdulla2001@gmail.com>
2025-06-25 03:20:34 +0300

    registered Post in admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from blog.models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_published', 'created_at', 'views_count')
+    list_filter = ('is_published', 'created_at')
+    search_fields = ('title', 'content')
+    ordering = ('-created_at',)
