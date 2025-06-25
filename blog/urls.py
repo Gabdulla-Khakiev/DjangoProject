f276f88eb78c61d93412c9ee587e9d018b3392b4
Gabdulla <gabdulla2001@gmail.com>
2025-06-25 13:18:25 +0300

    registered Post in admin.py

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ app_name = BlogConfig.name
 
 urlpatterns = [
     path('blog/', PostListView.as_view(), name='blog_list'),
-    path('blog_list/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('create/', PostCreateView.as_view(), name='post_create'),
     path('<int:pk>/edit/', PostUpdateView.as_view(), name='post_update'),
     path('<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
--- blog/views.py
+++ blog/views.py
@@ -29,7 +29,10 @@ class PostCreateView(CreateView):
 class PostUpdateView(UpdateView):
     model = Post
     form_class = PostForm
-    success_url = reverse_lazy('blog:blog_list')
+
+    def get_success_url(self):
+        return reverse_lazy('blog:post_detail', kwargs={'pk': self.object.pk})
+
 
 
 class PostDeleteView(DeleteView):
